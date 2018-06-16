1282352c75087c6667457a8d7d79617e5ebbcf91
shichengyuan <shiseimoto@gmail.com>
2018-06-16 17:11:58 +0800

    fjslkd

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -16,6 +16,7 @@ class Admin::OrdersController < ApplicationController
   def ship
     @order = Order.find(params[:id])
     @order.ship!
+    OrderMailer.notify_ship(@order).deliver!
     redirect_to :back
   end
 
@@ -29,6 +30,7 @@ class Admin::OrdersController < ApplicationController
   def cancel
     @order = Order.find(params[:id])
     @order.cancel_order!
+    OrderMailer.notify_cancel(@order).deliver!
     redirect_to :back
   end
 
@@ -37,5 +39,5 @@ class Admin::OrdersController < ApplicationController
     @order.return_good
     redirect_to :back
   end
-  
+
 end
